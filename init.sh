201aa306e9a9f0455c9daea9ce761871ef8637a1
Chanwit Kaewkasi <chanwit@gmail.com>
2022-04-01 20:57:02 +0700

    fix version in the init.sh

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@ bash -x ./init-gitops-engine.sh
 pushd .
 git clone https://github.com/argoproj/argo-cd
 cd argo-cd
-git checkout -b workspace v2.2.5
+git checkout -b workspace v2.2.8
 stg init
 stg import -t --series ../patches-argo-cd/series
 mv ../gitops-engine .
