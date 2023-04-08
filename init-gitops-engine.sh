0dbdc7ff87cf411f469419acd7bcb95844cc533d
Chanwit Kaewkasi <chanwit@gmail.com>
2023-04-08 13:12:38 +0000

    improve the build script to support stg 2.2.2
    Signed-off-by: Chanwit Kaewkasi <chanwit@gmail.com>

--- init-gitops-engine.sh
+++ init-gitops-engine.sh
@@ -6,4 +6,14 @@ cd gitops-engine
 git checkout -b workspace ${GITOPS_ENGINE_VERSION}
 stg init
 stg import -t --series ../patches-gitops-engine/series
+stg_version_output=$(stg --version | head -n 1)
+if [ "$stg_version_output" == "Stacked Git 2.2.2" ]; then
+	# for stg 2.2.2
+	SERIES=$(stg series --color never --all -P)
+	echo "$SERIES" | while read -r line; do
+	    cleaned_line=$(echo "$line" | sed -e 's/^[0-9]*-//')
+	    stg rename $line $cleaned_line
+	done
+if
+
 popd
--- init.sh
+++ init.sh
@@ -8,5 +8,15 @@ cd argo-cd
 git checkout -b workspace ${BASE_VERSION}
 stg init
 stg import -t --series ../patches-argo-cd/series
+stg_version_output=$(stg --version | head -n 1)
+if [ "$stg_version_output" == "Stacked Git 2.2.2" ]; then
+	# for stg 2.2.2
+	SERIES=$(stg series --color never --all -P)
+	echo "$SERIES" | while read -r line; do
+	    cleaned_line=$(echo "$line" | sed -e 's/^[0-9]*-//')
+	    stg rename $line $cleaned_line
+	done
+fi
+
 mv ../gitops-engine .
 popd
