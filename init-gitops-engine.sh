c6ee820c255173d562921443bff5561a2089e557
Chanwit Kaewkasi <chanwit@gmail.com>
2023-04-09 14:04:20 +0700

    fix stg version check
    Signed-off-by: Chanwit Kaewkasi <chanwit@gmail.com>

--- init-gitops-engine.sh
+++ init-gitops-engine.sh
@@ -6,7 +6,7 @@ cd gitops-engine
 git checkout -b workspace ${GITOPS_ENGINE_VERSION}
 stg init
 stg import -t --series ../patches-gitops-engine/series
-stg_version_output=$(stg --version | head -n 1)
+stg_version_output=$(stg --version | grep -i stacked | head -n 1)
 if [ "$stg_version_output" == "Stacked Git 2.2.2" ]; then
 	# for stg 2.2.2
 	SERIES=$(stg series --color never --all -P)
@@ -14,6 +14,6 @@ if [ "$stg_version_output" == "Stacked Git 2.2.2" ]; then
 	    cleaned_line=$(echo "$line" | sed -e 's/^[0-9]*-//')
 	    stg rename $line $cleaned_line
 	done
-if
+fi
 
 popd
--- init.sh
+++ init.sh
@@ -8,7 +8,7 @@ cd argo-cd
 git checkout -b workspace ${BASE_VERSION}
 stg init
 stg import -t --series ../patches-argo-cd/series
-stg_version_output=$(stg --version | head -n 1)
+stg_version_output=$(stg --version | grep -i stacked | head -n 1)
 if [ "$stg_version_output" == "Stacked Git 2.2.2" ]; then
 	# for stg 2.2.2
 	SERIES=$(stg series --color never --all -P)
